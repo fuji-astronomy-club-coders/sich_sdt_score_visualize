29ed3c21d6f9715c1957be350b4a9cbf0d7ed2d7
fujitenmon-coder <268108073+fujitenmon-coder@users.noreply.github.com>
2026-07-23 16:25:49 +0900

    refactor: コードの可読性向上のためインポート文を整理し、デバッグ出力を簡素化

--- std_visualize_100frame.py
+++ std_visualize_100frame.py
@@ -3,7 +3,12 @@ import cv2
 import sys
 import json
 import numpy as np
-from lib.lib_frommain import normalize_image, extract_sun_mini,create_colormap
+from lib.lib_frommain import (
+    normalize_image,
+    extract_sun_mini,
+    create_colormap,
+    statistics_image,
+)
 from lib.ffmpeg_video import compress_analysis_frames
 
 # パラメータ宣言
@@ -44,12 +49,9 @@ for start in range(0, n_frames, 100):
     # 最後は100枚未満でもそのまま処理
     section = frames[start:end]
 
+    st_ed.append((start,end))
     if DEBUG:
-        print(
-            f"{start}～{end-1} frame",
-            "shape:",
-            section.shape
-        )
+        print(f"{start}～{end-1} frame", "shape:", section.shape)
 
     # 100フレーム分の各ピクセルの標準偏差を計算
     std_image = np.std(section, axis=0)
@@ -60,24 +62,23 @@ for start in range(0, n_frames, 100):
 std_images = np.array(std_images)
 std_frames, height, width = std_images.shape
 
-#標準偏差画像の確認(デバッグ表示)
+# 標準偏差画像の確認(デバッグ表示)
 if DEBUG:
     print("===入力フレーム情報===")
-    print("データサイズ:",frames.shape)
+    print("データサイズ:", frames.shape)
     print("最小値:", frames.min())
     print("最大値:", frames.max())
     print("=====================")
-    
+
     print("\n=====標準偏差画像=====")
-    print("画像枚数:",std_frames)
-    print("画像サイズ:",(height, width))
+    print("画像枚数:", std_frames)
+    print("画像サイズ:", (height, width))
     print("=====================")
-    
-#カラーマップ作成
+
+# カラーマップ作成
 colormap_lut = create_colormap()
 
 # =====================動画作成用====================
-color_frames = []
 # 1フレームずつ取り出し、LUTを適用して保存用配列へ追加
 for frame in std_images:
     # 標準偏差を50〜100へ正規化
